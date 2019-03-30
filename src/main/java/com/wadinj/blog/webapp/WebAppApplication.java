090c64008d1769d616209d9955688b9742f2a5e4
Jonathan Wadin <wadin.jonathan@gmail.com>
2019-03-30 13:03:32 +0100

    (init) Setup project from spring initializr

---
+++ src/main/java/com/wadinj/blog/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.wadinj.blog.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
