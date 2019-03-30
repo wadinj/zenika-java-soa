162cd4d62bbe39a98dffd3c00d16fd3e7680f63d
Jonathan Wadin <wadin.jonathan@gmail.com>
2019-03-30 15:21:21 +0100

    (init) setup integration test on controller

--- src/test/java/com/wadinj/blog/webapp/WebAppApplicationTests.java
+++ src/test/java/com/wadinj/blog/webapp/WebAppApplicationTests.java
@@ -1,16 +1,42 @@
 package com.wadinj.blog.webapp;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		this.base = new URL("http://localhost:" + port + "/");
 	}
 
+	@Test
+	public void getHello() {
+
+		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+		assertThat(response.getBody(), equalTo("Greetings from Spring Boot!"));
+	}
 }
